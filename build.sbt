6bbc23368c44aa603bb425a7790a99cfbad02f23
swaggerloaf <swaggerloaf@gmail.com>
2022-03-18 17:44:53 -0400

    updates

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 
 // The simplest possible sbt build file is just one line:
 
-scalaVersion := "2.12.15"
+scalaVersion := "2.13.8"
 // That is, to create a valid sbt build, all you've got to do is define the
 // version of Scala you'd like your project to use.
 
@@ -23,6 +23,8 @@ version := "1.0"
 // You can define other libraries as dependencies in your build like this:
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.1"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "3.2.1"
+
 
 // Here, `libraryDependencies` is a set of dependencies, and by using `+=`,
 // we're adding the scala-parser-combinators dependency to the set of dependencies
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,30 @@
 /* SimpleApp.scala */
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.linalg.{Matrix, Vectors}
+import org.apache.spark.ml.stat.Correlation
+import org.apache.spark.sql.Row
+
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "README.md" // Should be some file on your system
+    val file = "./data/src/main/resources/people.json" // Should be some file on your system
     val spark = SparkSession.builder
       .appName("Simple Application")
       .config("spark.master", "local")
       .getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+
+    val data = Seq(
+      Vectors.sparse(4, Seq((0, 1.0), (3, -2.0))),
+      Vectors.dense(4.0, 5.0, 0.0, 3.0),
+      Vectors.dense(6.0, 7.0, 0.0, 8.0),
+      Vectors.sparse(4, Seq((0, 9.0), (3, 1.0)))
+    )
+
+    val df = data.map(Tuple1.apply).toDF("features")
+    val Row(coeff1: Matrix) = Correlation.corr(df, "features").head
+    println(s"Pearson correlation matrix:\n $coeff1")
+
+    val Row(coeff2: Matrix) = Correlation.corr(df, "features", "spearman").head
+    println(s"Spearman correlation matrix:\n $coeff2")
     spark.stop()
   }
 }
